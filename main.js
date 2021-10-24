7f8f0023b22fbeb3f9f54f982bb7915ee9a12e84
aaroncf2008 <aaroncf2008@gmail.com>
2021-10-23 20:57:50 -0400

    awf
    waf

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#typed', {
+  strings: ['<p class="header1">aaronfried</p>^400\n `<p class="header3">Content Developer / Jr. CyberSecurity Pentester / Jr. Software Engineer </p>` ^400\n\n <p class="header4">Certifications: None :(</p> ^400\n <p class="header4">Upcoming Certifications: eJPT, OSCP, eCPPTv2</p> ^400\n\n\n <p class="header5">Forked from n0n1m0us on GitHub</p> ^400\n'],
+  typeSpeed: 90,
+  backSpeed: 0,
+  loop: false,
+  cursorChar: '|',
+  fadeout: true,
+});
